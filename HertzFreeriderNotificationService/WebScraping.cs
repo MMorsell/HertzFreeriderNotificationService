48342fc46f7d8f01cbe614807df1ebdc7e48cca7
MMorsell <Erazap@LIVE.SE>
2019-09-07 19:39:05 +0200

    added-beginning-to-hertz-webscraping

---
+++ HertzFreeriderNotificationService/WebScraping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace HertzFreeriderNotificationService
+{
+    public class WebScraping
+    {
+        public bool DownloadNewVersion()
+        {
+            bool returnbool = false;
+            var webInterface = new HtmlWeb();
+
+            var regexMatchOnFalun = new Regex("target[=][\"]stationInfo[\"][>]Falun[<][/]");
+
+            return returnbool;
+        }
+    }
+}
